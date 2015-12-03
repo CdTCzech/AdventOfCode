9174b2105a7c0c071d0c6d022e095c39207988b4
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-12-03 17:51:01 +0100

    Day 1 Part 2

--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -20,9 +20,32 @@ void day1part1()
 	}
 }
 
+void day1part2()
+{
+	for (const auto& line : getLineByLine("day1.txt"))
+	{
+		int floor = 0, result = 0;
+
+		for (size_t i = 0; i < line.size(); ++i)
+		{
+			if (line[i] == '(') ++floor;
+			else --floor;
+
+			if (floor == -1)
+			{
+				result = i;
+				break;
+			}
+		}
+
+		std::cout << result + 1 << std::endl;
+	}
+}
+
 int main()
 {
 	std::cout << "Day1Part1: "; day1part1();
+	std::cout << "Day1Part2: "; day1part2();
 	system("pause");
 	return 0;
 }
