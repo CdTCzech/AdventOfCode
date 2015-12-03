eac8ec4bf5b09c04db124c63c8b5d7de8c5a4270
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-12-03 17:54:35 +0100

    Day 2 Part 1

--- AdventOfCode/FileReader.h
+++ AdventOfCode/FileReader.h
@@ -2,8 +2,23 @@
 
 #include <experimental/generator>
 #include <fstream>
+#include <functional>
 #include <string>
+#include <vector>
 
+
+template <typename T>
+std::experimental::generator<std::vector<T>> getLineByLine(std::string filename,
+	std::function<std::vector<T>(std::string&)> func = [](std::string& var) { return std::vector<T>(); })
+{
+	std::ifstream infile(filename);
+	std::string line;
+
+	while (getline(infile, line))
+	{
+		yield func(line);
+	}
+}
 
 auto getLineByLine(std::string filename)
 {
--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
+#include <sstream>
 #include "FileReader.h"
 
 
@@ -42,10 +42,38 @@ void day1part2()
 	}
 }
 
+void day2part1()
+{
+	auto result = 0;
+
+	for (const auto& line : getLineByLine<int>("day2.txt", [](std::string& var)
+	{
+		std::vector<int> dimensions;
+		std::replace(var.begin(), var.end(), 'x', ' ');
+		std::istringstream iss(var);
+		int l, w, h;
+		iss >> l >> w >> h;
+		dimensions.push_back(l);
+		dimensions.push_back(w);
+		dimensions.push_back(h);
+		return dimensions;
+	}))
+	{
+		auto sideA = line[0] * line[1];
+		auto sideB = line[0] * line[2];
+		auto sideC = line[1] * line[2];
+		auto sideM = std::min(sideA, std::min(sideB, sideC));
+		result += 2 * sideA + 2 * sideB + 2 * sideC + sideM;
+	}
+
+	std::cout << result << std::endl;
+}
+
 int main()
 {
 	std::cout << "Day1Part1: "; day1part1();
 	std::cout << "Day1Part2: "; day1part2();
+	std::cout << "Day2Part1: "; day2part1();
 	system("pause");
 	return 0;
 }
