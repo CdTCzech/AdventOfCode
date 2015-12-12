2e5080d7eb04ef71dca505ea1d1c739e2c8e89ad
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-12-12 22:22:05 +0100

    Day 12 Part 2

--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -721,6 +721,70 @@ namespace day12
 
 		std::cout << result << std::endl;
 	}
+
+	void part2()
+	{
+		const auto& line = getLine("day12.txt");
+		std::map<size_t, size_t> toSkip;
+		int result = 0;
+		std::string buffer;
+
+		for (size_t index = 0; index < line.size(); ++index)
+		{
+			if (line[index + 0] == ':' && line[index + 1] == '\"' && line[index + 2] == 'r' &&
+				line[index + 3] == 'e' && line[index + 4] == 'd' && line[index + 5] == '\"')
+			{
+				size_t start = index;
+				int counter = 0;
+
+				while (counter != -1)
+				{
+					--start;
+					if (start == -1) return;
+					if (line[start] == '{') --counter;
+					else if (line[start] == '}') ++counter;
+				}
+
+				counter = 0;
+
+				while (counter != -1)
+				{
+					++index;
+					if (index == line.size()) return;
+					if (line[index] == '{') ++counter;
+					else if (line[index] == '}') --counter;
+				}
+
+				toSkip.insert({ start, index });
+			}
+		}
+
+		for (size_t index = 0; index < line.size(); ++index)
+		{
+			if (toSkip.find(index) != toSkip.end())
+			{
+				if (!buffer.empty())
+				{
+					result += std::stoi(buffer);
+					buffer.clear();
+				}
+
+				index = toSkip.at(index);
+				continue;
+			}
+
+			if (line[index] == '-' && buffer.empty()) buffer += line[index];
+			else if (line[index] >= '0' && line[index] <= '9') buffer += line[index];
+			else if (!buffer.empty())
+			{
+				result += std::stoi(buffer);
+				buffer.clear();
+			}
+			else buffer.clear();
+		}
+
+		std::cout << result << std::endl;
+	}
 }
 
 int main()
@@ -746,6 +810,7 @@ int main()
 	std::cout << "Day 11 Part 1: "; day11::part1();
 	std::cout << "Day 11 Part 2: "; day11::part2();
 	std::cout << "Day 12 Part 1: "; day12::part1();
+	std::cout << "Day 12 Part 2: "; day12::part2();
 	system("pause");
 	return 0;
 }
