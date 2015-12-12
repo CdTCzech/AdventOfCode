4024e2641ac94c3cfd7abd3f7e43901a788559f2
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-12-12 10:34:04 +0100

    Get only one line for FileReader. Solving issues in Release mode in VS.

--- AdventOfCode/FileReader.h
+++ AdventOfCode/FileReader.h
@@ -7,6 +7,16 @@
 
 
 template <typename T = std::string>
+auto getLine(std::string filename, std::function<T(std::string&)> func = [](std::string& var) { return var; })
+{
+	std::ifstream infile(filename);
+	std::string line;
+
+	getline(infile, line);
+	return func(line);
+}
+
+template <typename T = std::string>
 auto getLineByLine(std::string filename, std::function<T(std::string&)> func = [] (std::string& var) { return var; } )
 {
 	std::ifstream infile(filename);
