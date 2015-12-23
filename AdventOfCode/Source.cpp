da2d2871bce65540baf2524e672adc7cbce7dcae
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-12-23 17:22:47 +0100

    Day 19 Part 1

--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -1449,6 +1449,66 @@ namespace day18
 	}
 }
 
+namespace day19
+{
+	void part1()
+	{
+		std::map<std::string, std::vector<std::string>> replacements;
+		std::set<std::string> result;
+
+		for (const auto& line : getLineByLine<std::vector<std::string>>("day19_1.txt", [&replacements](std::string& var)
+		{
+			std::istringstream iss(var);
+			std::vector<std::string> splitted(std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{});
+			return splitted;
+		}))
+		{
+			if (replacements.find(line[0]) == replacements.end()) replacements.insert({ line[0], { line[2] } });
+			else replacements.at(line[0]).push_back(line[2]);
+		}
+
+		auto line = getLine("day19_2.txt");
+
+		for (size_t i = 0; i < line.size() - 1; ++i)
+		{
+			std::string key{ line[i] };
+			if (replacements.find(key) != replacements.end())
+			{
+				for (const auto& replacement : replacements.at(key))
+				{
+					auto temp = line;
+					temp.replace(i, 1, replacement);
+					result.insert(temp);
+				}
+			}
+
+			key += line[i + 1];
+			if (replacements.find(key) != replacements.end())
+			{
+				for (const auto& replacement : replacements.at(key))
+				{
+					auto temp = line;
+					temp.replace(i, 2, replacement);
+					result.insert(temp);
+				}
+			}
+		}
+
+		std::string key{ line[line.size() - 1] };
+		if (replacements.find(key) != replacements.end())
+		{
+			for (const auto& replacement : replacements.at(key))
+			{
+				auto temp = line;
+				temp.replace(line.size() - 1, 1, replacement);
+				result.insert(temp);
+			}
+		}
+
+		std::cout << result.size() << std::endl;
+	}
+}
+
 int main()
 {
 	std::cout << "Day 1 Part 1: "; day1::part1();
@@ -1487,6 +1547,7 @@ int main()
 	std::cout << "Day 17 Part 2: "; day17::part2();
 	std::cout << "Day 18 Part 1: "; day18::part1();
 	std::cout << "Day 18 Part 2: "; day18::part2();
+	std::cout << "Day 19 Part 1: "; day19::part1();
 	system("pause");
 	return 0;
 }
