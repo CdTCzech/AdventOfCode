b45362fb7a504dc300ca89e9df7938574ff04279
Miroslav Hrnčíř <hrncirmirek@windowslive.com>
2015-12-23 12:17:45 +0100

    Day 18 Part 2

--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -1363,11 +1363,95 @@ namespace day18
 
 		std::cout << result << std::endl;
 	}
+
+	void part2()
+	{
+		std::array<std::array<char, 102>, 102> one;
+		std::array<std::array<char, 102>, 102> two;
+
+		for (size_t i = 0; i < 102; ++i)
+		{
+			one[i][0] = '.';
+			two[i][0] = '.';
+
+			one[0][i] = '.';
+			two[0][i] = '.';
+
+			one[i][101] = '.';
+			two[i][101] = '.';
+
+			one[101][i] = '.';
+			two[101][i] = '.';
+		}
+
+		size_t lineNumber = 1;
+
+		for (const auto& line : getLineByLine("day18.txt"))
+		{
+			for (size_t i = 0; i < line.size(); ++i) one[lineNumber][i + 1] = line[i];
+			++lineNumber;
+		}
+
+		one[1][1] = '#';
+		one[1][100] = '#';
+		one[100][1] = '#';
+		one[100][100] = '#';
+
+		auto* read = &one;
+		auto* write = &two;
+
+		for (int i = 0; i < 100; ++i)
+		{
+			if (i % 2 == 1)
+			{
+				read = &two;
+				write = &one;
+			}
+			else
+			{
+				read = &one;
+				write = &two;
+			}
+
+			for (size_t y = 1; y < 101; ++y)
+			{
+				for (size_t x = 1; x < 101; ++x)
+				{
+					unsigned int neighboursOn = 0;
+					if ((*read)[y + 1][x + 1] == '#') ++neighboursOn;
+					if ((*read)[y][x + 1] == '#') ++neighboursOn;
+					if ((*read)[y - 1][x + 1] == '#') ++neighboursOn;
+					if ((*read)[y - 1][x] == '#') ++neighboursOn;
+					if ((*read)[y - 1][x - 1] == '#') ++neighboursOn;
+					if ((*read)[y][x - 1] == '#') ++neighboursOn;
+					if ((*read)[y + 1][x - 1] == '#') ++neighboursOn;
+					if ((*read)[y + 1][x] == '#') ++neighboursOn;
+
+					if (((*read)[y][x] == '#') && (neighboursOn == 2 || neighboursOn == 3)) (*write)[y][x] = '#';
+					else if (((*read)[y][x] == '.') && (neighboursOn == 3)) (*write)[y][x] = '#';
+					else (*write)[y][x] = '.';
+				}
+			}
+
+			(*write)[1][1] = '#';
+			(*write)[1][100] = '#';
+			(*write)[100][1] = '#';
+			(*write)[100][100] = '#';
+		}
+
+		unsigned int result = 0;
+		for (const auto& line : one)
+		{
+			for (const auto& element : line) if (element == '#') ++result;
+		}
+
+		std::cout << result << std::endl;
+	}
 }
 
 int main()
 {
-	/*std::cout << "Day 1 Part 1: "; day1::part1();
+	std::cout << "Day 1 Part 1: "; day1::part1();
 	std::cout << "Day 1 Part 2: "; day1::part2();
 	std::cout << "Day 2 Part 1: "; day2::part1();
 	std::cout << "Day 2 Part 2: "; day2::part2();
@@ -1400,8 +1484,9 @@ int main()
 	std::cout << "Day 16 Part 1: "; day16::part1();
 	std::cout << "Day 16 Part 2: "; day16::part2();
 	std::cout << "Day 17 Part 1: "; day17::part1();
-	std::cout << "Day 17 Part 2: "; day17::part2();*/
+	std::cout << "Day 17 Part 2: "; day17::part2();
 	std::cout << "Day 18 Part 1: "; day18::part1();
+	std::cout << "Day 18 Part 2: "; day18::part2();
 	system("pause");
 	return 0;
 }
