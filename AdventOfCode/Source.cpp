5473f12a63bd11be2769d87824db281512c3c70e
CdTCzech <hrncirmirek@windowslive.com>
2016-10-04 10:22:59 +0200

    Day 22 Part 1

--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -1711,9 +1711,85 @@ namespace day21
 	}
 }
 
+namespace day22
+{
+	struct Player
+	{
+		int HP;
+		int MAN;
+	};
+
+	struct Boss
+	{
+		int HP;
+		int DMG;
+	};
+
+	int simulate(bool playerTurn, Player player, Boss boss, int s, int p, int r)
+	{
+		if (p)
+		{
+			boss.HP -= 3;
+			--p;
+		}
+
+		if (player.HP <= 0) return 99999;
+		if (boss.HP <= 0) return 0;
+
+		if (r)
+		{
+			player.MAN += 101;
+			--r;
+		}
+
+		if (playerTurn)
+		{
+			int results[]{ 99999, 99999, 99999, 99999, 99999 };
+			if (s) --s;
+
+			// MAGIC MISSILE
+			if (player.MAN >= 53)
+			{
+				results[0] = 53 + simulate(false, { player.HP, player.MAN - 53 }, { boss.HP - 4, boss.DMG }, s, p, r);
+			}
+			// DRAIN
+			if (player.MAN >= 73)
+			{
+				results[1] = 73 + simulate(false, { player.HP + 2, player.MAN - 73 }, { boss.HP - 2, boss.DMG }, s, p, r);
+			}
+			// SHIELD
+			if (player.MAN >= 113 && !s)
+			{
+				results[2] = 113 + simulate(false, { player.HP, player.MAN - 113 }, { boss.HP, boss.DMG }, 6, p, r);
+			}
+			// POISON
+			if (player.MAN >= 173 && !p)
+			{
+				results[3] = 173 + simulate(false, { player.HP, player.MAN - 173 }, { boss.HP, boss.DMG }, s, 6, r);
+			}
+			// RECHARGE
+			if (player.MAN >= 229 && !r)
+			{
+				results[4] = 229 + simulate(false, { player.HP, player.MAN - 229 }, { boss.HP, boss.DMG }, s, p, 5);
+			}
+
+			return *std::min_element(results, results + 5);
+		}
+		else
+		{
+			return simulate(true, { player.HP - boss.DMG + (s ? 7 : 0) , player.MAN }, { boss.HP, boss.DMG }, (s ? s - 1 : s), p, r);
+		}
+	}
+
+	void part1()
+	{
+		std::cout << simulate(true, { 50, 500 }, { 71, 10 }, 0, 0, 0) << std::endl;
+	}
+}
+
 int main()
 {
-	/*std::cout << "Day 1 Part 1: "; day1::part1();
+	std::cout << "Day 1 Part 1: "; day1::part1();
 	std::cout << "Day 1 Part 2: "; day1::part2();
 	std::cout << "Day 2 Part 1: "; day2::part1();
 	std::cout << "Day 2 Part 2: "; day2::part2();
@@ -1752,9 +1828,10 @@ int main()
 	std::cout << "Day 19 Part 1: "; day19::part1();
 	std::cout << "Day 19 Part 2: "; day19::part2();
 	std::cout << "Day 20 Part 1: "; day20::part1();
-	std::cout << "Day 20 Part 2: "; day20::part2();*/
+	std::cout << "Day 20 Part 2: "; day20::part2();
 	std::cout << "Day 21 Part 1: "; day21::part1();
-	std::cout << "Day 21 Part 1: "; day21::part2();
+	std::cout << "Day 21 Part 2: "; day21::part2();
+	std::cout << "Day 22 Part 1: "; day22::part1();
 	system("pause");
 	return 0;
 }
